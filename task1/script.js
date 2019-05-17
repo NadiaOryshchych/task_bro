fe6b5d82e0741d375fe569668eead214652e6f0f
Nadia <nadia.naka.ne@gmail.com>
2019-05-17 22:22:38 +0300

    first task

--- task1/script.js
+++ task1/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', function () {
    'use strict';
    
-   let boxWrap = document.querySelector('.box-wrap'),
+   let app = document.querySelector('.app'),
        minusCol = document.querySelector('.minus-col'),
        minusRow = document.querySelector('.minus-row'),
        plusCol = document.querySelector('.plus-col'),
@@ -9,43 +9,20 @@ window.addEventListener('DOMContentLoaded', function () {
 
    addEvents();  
    function addEvents() {
-      const boxes = document.querySelectorAll('.box');
-      for (let i = 0; i < boxes.length; i++) {
-         // або можна mousemove
-         boxes[i].addEventListener('mouseenter', function () {
-            showMinusRow();
-            minusRow.style.top = this.offsetTop + 'px';
-            showMinusCol();
-            minusCol.style.left = this.offsetLeft + 'px ';
-         });
-      }
-
-      minusCol.addEventListener('mouseenter', function () {
-         showMinusRow();
-         showMinusCol();
+      app.addEventListener('mousemove', function (event) {
+         showMinus();
+         if (event.target.className == 'box') {
+            minusRow.style.top = event.target.offsetTop - 1 + 'px';
+            minusCol.style.left = event.target.offsetLeft - 1 + 'px';
+         }
       });
-      minusCol.addEventListener('mouseleave', hideMinus);
-
-      minusRow.addEventListener('mouseenter', function () {
-         showMinusRow();
-         showMinusCol();
-      });
-      minusRow.addEventListener('mouseleave', hideMinus);
-
-      boxWrap.addEventListener('mouseenter', function () {
-         showMinusRow();
-         showMinusCol();
-      });
-      boxWrap.addEventListener('mouseleave', hideMinus);
+      app.addEventListener('mouseleave', hideMinus);   
    }
-
-   function showMinusRow() {
+   function showMinus() {
       const boxRows = document.querySelectorAll('.box-row');
       if (boxRows.length > 1) {
          minusRow.style.display = 'flex';
       }
-   }
-   function showMinusCol() {
       const boxCol = document.querySelectorAll('.box-row .box:nth-child(2)');
       if (boxCol.length != 0) {
          minusCol.style.display = 'flex';
@@ -73,27 +50,29 @@ window.addEventListener('DOMContentLoaded', function () {
       const row = document.createElement('div');
       row.className = "box-row";
       row.innerHTML = boxRows[0].innerHTML;
-      boxWrap.appendChild(row);
+      app.appendChild(row);
       addEvents();
    });
 
    // delRow
    minusRow.addEventListener('click', function() {
-      const indexRow = Math.floor(this.offsetTop / plusCol.offsetHeight) - 1;
+      const indexRow = Math.floor(this.offsetTop / plusCol.offsetHeight);
       const boxRows = document.getElementsByClassName('box-row');
       const delRow = boxRows[indexRow];
       delRow.parentNode.removeChild(delRow);
       hideMinus();
+      this.style.top = (parseInt(this.style.top) - plusCol.offsetHeight + 1) + 'px';
    });
 
    // delCol
    minusCol.addEventListener('click', function() {
-      const indexCol = Math.floor(this.offsetLeft / plusCol.offsetWidth);
+      const indexCol = Math.floor(this.offsetLeft / plusCol.offsetWidth) + 2;
       const boxDel = document.querySelectorAll(`.box-row .box:nth-child(${indexCol})`);
       for (let i = 0; i < boxDel.length; i++) {
          const delCol = boxDel[i];
          delCol.parentNode.removeChild(delCol);
       }
       hideMinus();
+      this.style.left = (parseInt(this.style.left) - plusCol.offsetWidth + 2) + 'px';
    });
 });
