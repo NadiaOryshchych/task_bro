614e15e4303296d255c13fe6e855d25a176df9c7
Nadia <nadia.naka.ne@gmail.com>
2019-08-28 00:30:40 +0300

    testing app, srep 1

--- task3/src/index.test.js
+++ task3/src/index.test.js
@@ -2,38 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import Table from './components/Table';
-import renderer from 'react-test-renderer';
+// import renderer from 'react-test-renderer';
 import {shallow} from 'enzyme';
 
-describe('Testing <Table /> initial', () => {
-  const table = shallow( <Table /> )
-
-  it('render initial Table', () => {
-    expect(table.find('div.table > div.row')).to.have.lengthOf(4);
-    // expect(wrapper.find('.icon-star')).to.have.lengthOf(1);
-  });
-});
-
 describe('Testing <App />', () => {
+
+  // beforeEach(() => {
+  //   app.instance().render();
+  // });
 
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render( <App /> , div);
-  });
+  }); 
   // або можна написати ось так
   xit('renders without crashing', () => {
     shallow(<App />);
   });
   
-  const app = shallow(<App />);
-  // const table = shallow(<Table />); 
-
-  it('renders three <Table /> components', () => {
+  it('testing app has table', () => {
+    const app = shallow(<App />);
     expect(app.find(Table)).toHaveLength(1);
   });
 
-  it('testing height Table', () => {
-    expect(app.find('div.table > div.row')).toHaveLength(4);
+  // нужно задать инишиал валюас и построить апп свой - найти в нем таблицу - посчитать количество строк и колонок
+  it('render initial Table', () => {
+    const app = shallow(<App initialWidth={4} initialHeight={5} />);
+    const table = shallow( <Table /> );
+    expect(app.find(Table).length).toBe(1);
+    expect(table.find('.row').length).toBe(5);
   });
 
   // записую змінну
